56592d10cba0c6d0148bd065f4cc2d4070879d2f
Rubens Ramos <rubensframos00@gmail.com>
2021-08-02 15:52:36 -0300

    adicionando novas rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const {Router} = require('express');
 const router = Router();
 
-const { getTargets, createTarget, getTargetsById, deleteTarget, updateTarget, getRun, getUsersById, createUser } = require('../controllers/index.controller')
+const { getTargets, createTarget, getTargetsById, deleteTarget, updateTarget, getRun, getUsersById, createUser, getAssets, getAssetsById, getAports, getAportsById } = require('../controllers/index.controller')
 router.get('/', getRun);
 router.get('/targets', getTargets);
 router.get('/targets/:id', getTargetsById);
@@ -10,5 +10,9 @@ router.delete('/targets/:id', deleteTarget);
 router.put('/targets/:id', updateTarget);
 router.get('/users/:id', getUsersById);
 router.post('/users', createUser);
+router.get('/assets', getAssets);
+router.get('/assets/:id', getAssetsById);
+router.get('/aport', getAports);
+router.get('/aport/:id', getAportsById);
 
 module.exports = router;
